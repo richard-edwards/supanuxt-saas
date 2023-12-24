194a2647a44ac438ea4595efdf02752247aacdf7
Richard Edwards <richard@richardedwards.ca>
2023-12-24 14:53:06 -0500

    refactor: add try/catch to handle unforseen errors

--- server/api/note.ts
+++ server/api/note.ts
@@ -4,20 +4,27 @@ import NotesService from '~/lib/services/notes.service';
 
 // Example API Route with query params ... /api/note?note_id=41
 export default defineProtectedEventHandler(async (event: H3Event) => {
-  const queryParams = getQuery(event);
-  let note_id: string = '';
-  if (queryParams.note_id) {
-    if (Array.isArray(queryParams.note_id)) {
-      note_id = queryParams.note_id[0];
-    } else {
-      note_id = queryParams.note_id.toString();
+  try {
+    const queryParams = getQuery(event);
+    let note_id: string = '';
+    if (queryParams.note_id) {
+      if (Array.isArray(queryParams.note_id)) {
+        note_id = queryParams.note_id[0];
+      } else {
+        note_id = queryParams.note_id.toString();
+      }
     }
+
+    const notesService = new NotesService();
+    const note = await notesService.getNoteById(+note_id);
+
+    return {
+      note
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      error: 'An error occurred while fetching the note.'
+    };
   }
-
-  const notesService = new NotesService();
-  const note = await notesService.getNoteById(+note_id);
-
-  return {
-    note
-  };
 });
